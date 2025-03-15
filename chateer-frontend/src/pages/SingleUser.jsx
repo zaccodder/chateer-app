d4606678d5c2edf9e45ee5cc41febbbada585ac4
modibokeita <modibokeita9390@gmail.com>
2025-03-15 21:28:14 +0200

    getting a single user

--- chateer-frontend/src/pages/SingleUser.jsx
+++ chateer-frontend/src/pages/SingleUser.jsx
@@ -15,7 +15,7 @@ export default function SingleUser() {
 
   }
   return (
-    <div className="flex flex-col mt-20 h-[75vh] max-w-lg mx-auto p-4 rounded-lg shadow-md">
+    <div className="flex flex-col mt-5 h-[75vh] max-w-lg mx-auto p-4 rounded-lg shadow-md">
       <div className="flex items-center gap-5 mb-10">
         <Link to="/chatroom"><IoIosArrowBack className="text-2xl"/></Link>
       <UserProfile />
@@ -34,12 +34,14 @@ export default function SingleUser() {
       </div>
 
       <form action={chandleClik} className="flex items-center space-x-2 p-2 ">
+        <div>
         <input
           type="text"
           placeholder="Type a message..."
           name="message"
           className="flex-grow p-2 border rounded-md"
         />
+        </div>
         <button className="px-4 py-2 bg-blue-500 text-white rounded-md">
           Send
         </button>
